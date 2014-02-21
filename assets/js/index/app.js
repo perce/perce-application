cca0101b1164818a475fac96a5007214573b4250
haeringer <christian@haeringer.net>
2014-02-21 21:53:06 +0100

    login wip

--- assets/js/index/app.js
+++ assets/js/index/app.js
@@ -22,7 +22,8 @@
           templateUrl : 'views/index/contact.html',
         } )
         .when( '/user', {
-          templateUrl : 'views/index/user.html'
+          templateUrl : 'views/index/user.html',
+          controller  : 'CreateUserController'
         } )
         .when( '/login', {
           templateUrl : 'views/index/login.html',
@@ -37,8 +38,36 @@
     } ] );
 
   indexApp.controller( 'LoginController', function( $scope, $http ) {
+    console.log( 'LoginController() ' );
+
     $scope.submit = function() {
-      console.log( $scope );
+      console.log( 'LoginController() ', $scope );
+
+      $http.post('phones/phones.json').success(function(data) {
+        $scope.phones = data;
+      });
+    };
+  } );
+
+
+
+  indexApp.controller( 'CreateUserController', function( $scope, $http ) {
+    console.log( 'CreateUserController() ' );
+
+    $scope.submit = function( event ) {
+      console.log( 'CreateUserController() ', $scope );
+
+      $http.post( '/user', $scope.data )
+          .success( function( data ){
+            console.log( 'login success! ', data );
+
+            if ( data.url ){
+              document.location = data.url;
+            }
+          } )
+          .error( function( data ){
+            console.warn( 'oh nooooooo :( login error! ', data );
+          } );
     };
   } );
 } )( angular );
